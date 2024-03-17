aaeecf54da4894e2d8df1f2989d737cf679b2d08
Gnimuc <qiyupei@gmail.com>
2024-03-17 22:47:31 +0900

    Add error handling

--- include/carotid/Utility.h
+++ include/carotid/Utility.h
@@ -2,6 +2,7 @@
 #define CAROTID_UTILITY_H
 
 #include <Eigen/Dense>
+#include <expected>
 #include <filesystem>
 #include <fstream>
 
@@ -13,15 +14,22 @@ template <typename T>
 using Mat = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
 
 template <typename T>
-[[nodiscard]] Mat<T> loadBinaryIntoMatrix(const Path &filename, int rows, int cols) {
+[[nodiscard]] std::expected<Mat<T>, std::ios_base::failure>
+loadBinaryIntoMatrix(const Path &filename, int rows, int cols) {
   std::ifstream in(filename, std::ios::binary);
   if (!in.is_open()) {
-    throw std::runtime_error("Cannot open file: " + filename.string());
+    return std::unexpected(
+        std::ios_base::failure("File not found or could not be opened.",
+                               std::make_error_code(std::errc::io_error)));
   }
 
   Mat<T> mat(rows, cols);
 
   in.read(reinterpret_cast<char *>(mat.data()), rows * cols * sizeof(T));
+  if (!in) {
+    return std::unexpected(std::ios_base::failure(
+        "Failed to read the entire file.", std::make_error_code(std::errc::io_error)));
+  }
 
   return mat;
 }
--- tests/Core/Core.cpp
+++ tests/Core/Core.cpp
@@ -8,8 +8,10 @@ TEST_CASE("Core", "[Utility]") {
   auto sanityBin = projectSrcDir / "tests" / "Core" / "f32.bin";
 
   SECTION("Data loading sanity check") {
-    auto mat = loadBinaryIntoMatrix<float>(sanityBin, 2, 3);
+    auto result = loadBinaryIntoMatrix<float>(sanityBin, 2, 3);
+    REQUIRE(result.has_value());
 
+    auto mat = result.value();
     Eigen::IOFormat OctaveFmt(Eigen::StreamPrecision, 0, ", ", ";\n", "", "", "[", "]");
     INFO("Matrix:\n" << mat.format(OctaveFmt));
 
@@ -19,5 +21,15 @@ TEST_CASE("Core", "[Utility]") {
     REQUIRE(mat(1, 0) == 4.0f);
     REQUIRE(mat(1, 1) == 5.0f);
     REQUIRE(mat(1, 2) == 6.0f);
+  }
+
+  SECTION("Data loading failure check") {
+    auto result = loadBinaryIntoMatrix<float>("", 2, 4);
+    CAPTURE(result.error().what());
+    REQUIRE_FALSE(result.has_value());
+
+    result = loadBinaryIntoMatrix<float>(sanityBin, 2, 4);
+    CAPTURE(result.error().what());
+    REQUIRE_FALSE(result.has_value());
   }
 }
