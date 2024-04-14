188896f0b902f82019fc763be7d9a874eb823f35
Gnimuc <Gnimuc@users.noreply.github.com>
2024-04-14 14:41:51 +0000

    deploy: 6aa3892b12dc2df67cfb3f022950c390cdf8b7fd

--- search/variables_0.js
+++ search/variables_0.js
@@ -3,5 +3,7 @@ var searchData=
   ['carotid_5fspatial_5fdimensions_0',['CAROTID_SPATIAL_DIMENSIONS',['../namespacecarotid.html#a41fbb103fe03425cee752e91c8818f39',1,'carotid']]],
   ['carotid_5fvertex_5fcount_5flvl1_1',['CAROTID_VERTEX_COUNT_LVL1',['../namespacecarotid.html#aee5eabf6198f41ae9352be8f6f1eb625',1,'carotid']]],
   ['carotid_5fvertex_5fcount_5flvl2_2',['CAROTID_VERTEX_COUNT_LVL2',['../namespacecarotid.html#ac7780ea97e4fb63d6db1624b33c16b9b',1,'carotid']]],
-  ['carotid_5fvertex_5fcount_5flvl3_3',['CAROTID_VERTEX_COUNT_LVL3',['../namespacecarotid.html#ac2c4ae0013ada080c7a400d40ece8f66',1,'carotid']]]
+  ['carotid_5fvertex_5fcount_5flvl3_3',['CAROTID_VERTEX_COUNT_LVL3',['../namespacecarotid.html#ac2c4ae0013ada080c7a400d40ece8f66',1,'carotid']]],
+  ['cur_5fmodel_4',['cur_model',['../classcarotid_1_1_identity_generator.html#a6f11c083c637c485b16b591fba8918af',1,'carotid::IdentityGenerator']]],
+  ['cur_5fparams_5',['cur_params',['../classcarotid_1_1_identity_generator.html#a637279cef77c4ed7baabf6165ef15138',1,'carotid::IdentityGenerator']]]
 ];
